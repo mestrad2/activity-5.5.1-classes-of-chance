4c3530d5e1444cd5eb2058cced7ee045554bc1e2
Manuel Estrada <manuel.estrada16@gmail.com>
2022-08-06 11:11:52 -0400

    initial commit

--- app.js
+++ app.js
@@ -1,24 +1,44 @@
 class Casino {
-    // Write code here
+    constructor(name, fakeCoin) {
+        this.timesPlayed = 0;
+        this.money = 0;
+        this.name = name;
+        this.fakeCoin = fakeCoin;
+    }
+
+    playGame(betAmount) {
+
+        this.timesPlayed += 1;
+
+        if(Math.random() >= 0.5 && this.fakeCoin == false) {
+            console.log("Player wins " + betAmount * (this.timesPlayed) + " dollars!");
+            this.money += betAmount * this.timesPlayed;
+            console.log("Current Winnings: " + this.money);
+        } else {
+            console.log(this.name + " wins!");
+        }
+
+        
+    }
 };
 
-// TESTS
+/* TESTS
 const myCasino = new Casino("HackerU Casino");
 console.log(myCasino);
 myCasino.playGame(5);
 myCasino.playGame(15);
 myCasino.playGame(25);
-myCasino.playGame(35);
+myCasino.playGame(35); */
 
 // BONUS TESTS
-/*
-const myBonusCasino = new Casino("HackerU Bonus Casino", true);
+
+const myBonusCasino = new Casino("HackerU Bonus Casino", false);
 console.log(myBonusCasino);
 myBonusCasino.playGame(5);
 myBonusCasino.playGame(15);
 myBonusCasino.playGame(25);
 myBonusCasino.playGame(35);
-*/
+
 
 // Extra BONUS TESTS
 /*
